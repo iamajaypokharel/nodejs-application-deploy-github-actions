bf0eee83ac42394c1c0e5addcf8af5fe1bec30c8
Ajay Pokharel <ajaypokharel444@gmail.com>
2026-05-13 17:14:21 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app =express()
+const PORT = process.env.PORT || 8080
+
+app.get('/', (req,res)=> {
+     return res.json({msg:'hello from server'})
+    
+})
+app.listen(PORT,() =>{
+    console.log('SERVER IS UP AND RUNNING ON PORT'
+        
+    )
+})
